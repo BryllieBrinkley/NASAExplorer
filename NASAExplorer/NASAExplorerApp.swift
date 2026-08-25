2edf7d7ca690dfd022276c4d0e5f1b3c6d78f179
Jibryll Brinkley <jibryll.s.brinkley@gmail.com>
2026-08-25 12:16:55 -0400

    Initial Commit

---
+++ NASAExplorer/NASAExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NASAExplorerApp.swift
+//  NASAExplorer
+//
+//  Created by Jibryll Brinkley on 8/25/26.
+//
+
+import SwiftUI
+
+@main
+struct NASAExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
