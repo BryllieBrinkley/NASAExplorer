4e55b8098350b0e029508dfce3a7936e9012ac30
Jibryll Brinkley <jibryll.s.brinkley@gmail.com>
2026-08-31 17:48:06 -0400

    made custom AppBackground() to place stars on bg

---
+++ NASAExplorer/AppBackground.swift
@@ -0,0 +1,18 @@
+//
+//  AppBAckground.swift
+//  NASAExplorer
+//
+//  Created by Jibryll Brinkley on 8/31/26.
+//
+
+import SwiftUI
+
+struct AppBAckground: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppBAckground()
+}
